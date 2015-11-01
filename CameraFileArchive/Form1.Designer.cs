48c0dcc3f9dccf1618386ea392261e2c7aa99b68
A Slatius <a.slatius@gmail.com>
2015-11-01 13:19:08 +0100

    Initial import

---
+++ CameraFileArchive/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CameraFileArchive
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSrc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDst = new System.Windows.Forms.TextBox();
+            this.BrowseSrcButton = new System.Windows.Forms.Button();
+            this.BrowseDstButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.progress = new System.Windows.Forms.Label();
+            this.bw = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source directory";
+            // 
+            // textBoxSrc
+            // 
+            this.textBoxSrc.Location = new System.Drawing.Point(116, 6);
+            this.textBoxSrc.Name = "textBoxSrc";
+            this.textBoxSrc.Size = new System.Drawing.Size(211, 20);
+            this.textBoxSrc.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination directory";
+            // 
+            // textBoxDst
+            // 
+            this.textBoxDst.Location = new System.Drawing.Point(116, 35);
+            this.textBoxDst.Name = "textBoxDst";
+            this.textBoxDst.Size = new System.Drawing.Size(211, 20);
+            this.textBoxDst.TabIndex = 4;
+            // 
+            // BrowseSrcButton
+            // 
+            this.BrowseSrcButton.Location = new System.Drawing.Point(333, 4);
+            this.BrowseSrcButton.Name = "BrowseSrcButton";
+            this.BrowseSrcButton.Size = new System.Drawing.Size(59, 23);
+            this.BrowseSrcButton.TabIndex = 3;
+            this.BrowseSrcButton.Text = "Browse...";
+            this.BrowseSrcButton.UseVisualStyleBackColor = true;
+            this.BrowseSrcButton.Click += new System.EventHandler(this.BrowseSrcButton_Click);
+            // 
+            // BrowseDstButton
+            // 
+            this.BrowseDstButton.Location = new System.Drawing.Point(333, 34);
+            this.BrowseDstButton.Name = "BrowseDstButton";
+            this.BrowseDstButton.Size = new System.Drawing.Size(59, 23);
+            this.BrowseDstButton.TabIndex = 5;
+            this.BrowseDstButton.Text = "Browse...";
+            this.BrowseDstButton.UseVisualStyleBackColor = true;
+            this.BrowseDstButton.Click += new System.EventHandler(this.BrowseDstButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(116, 64);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(211, 23);
+            this.progressBar.TabIndex = 6;
+            this.progressBar.Visible = false;
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(333, 64);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(59, 23);
+            this.GoButton.TabIndex = 1;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.HorizontalScrollbar = true;
+            this.listBox.Location = new System.Drawing.Point(10, 94);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(382, 56);
+            this.listBox.TabIndex = 6;
+            // 
+            // progress
+            // 
+            this.progress.AutoSize = true;
+            this.progress.Location = new System.Drawing.Point(7, 69);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(104, 13);
+            this.progress.TabIndex = 9;
+            this.progress.Text = "progess....................";
+            this.progress.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.progress.Visible = false;
+            // 
+            // bw
+            // 
+            this.bw.WorkerReportsProgress = true;
+            this.bw.WorkerSupportsCancellation = true;
+            this.bw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_DoWork);
+            this.bw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_RunWorkerCompleted);
+            this.bw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_ProgressChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 160);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.BrowseDstButton);
+            this.Controls.Add(this.BrowseSrcButton);
+            this.Controls.Add(this.textBoxDst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSrc);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Camera Photo Backup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSrc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDst;
+        private System.Windows.Forms.Button BrowseSrcButton;
+        private System.Windows.Forms.Button BrowseDstButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label progress;
+        private System.ComponentModel.BackgroundWorker bw;
+    }
+}
+
---
+++ CameraFileArchive/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace CameraFileArchive
+{
+    public partial class MainForm : Form
+    {
+        private RegistryKey regKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\CameraPhotoArchive");
+        private FileInfo[] jpgFiles;
+        private DateTime lastProgressUpdate;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxSrc.Text = (string)regKey.GetValue("LastSrc", "");
+            textBoxDst.Text = (string)regKey.GetValue("LastDst", "");
+        }
+
+        private void BrowseSrcButton_Click(object sender, EventArgs e)
+        {
+            if (textBoxSrc.Text != "")
+                folderBrowserDialog.SelectedPath = textBoxSrc.Text;
+
+            folderBrowserDialog.ShowNewFolderButton = false;
+            folderBrowserDialog.ShowDialog();
+            textBoxSrc.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void BrowseDstButton_Click(object sender, EventArgs e)
+        {
+            if (textBoxDst.Text != "")
+                folderBrowserDialog.SelectedPath = textBoxDst.Text;
+
+            folderBrowserDialog.ShowNewFolderButton = true;
+            folderBrowserDialog.ShowDialog();
+            textBoxDst.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = DialogResult.Yes;
+
+            if ("Go" == GoButton.Text)
+            {
+                if (Directory.Exists(textBoxSrc.Text) == false)
+                {
+                    MessageBox.Show("The source path does not exsist!",
+                                "Check path", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                if (Directory.Exists(textBoxDst.Text) == false)
+                {
+                    MessageBox.Show("The destination path does not exsist!",
+                                "Check path", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                if (textBoxSrc.Text.Contains("DCIM") != true)
+                {
+                    result = MessageBox.Show("The path does not contain 'DCIM' " +
+                                "which is normal for a camera memory card.\n\n" +
+                                "Are you sure you want to continue?",
+                                "Check path", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.No)
+                        return;
+                }
+                if (textBoxDst.Text.Substring(textBoxDst.Text.Length - 1) != "\\")
+                {
+                    textBoxDst.Text = textBoxDst.Text + "\\";
+                }
+                DirectoryInfo di = new DirectoryInfo(textBoxSrc.Text);
+                jpgFiles = di.GetFiles("*.jpg");
+
+                //Store last used paths...
+                regKey.SetValue("LastSrc", textBoxSrc.Text, RegistryValueKind.String);
+                regKey.SetValue("LastDst", textBoxDst.Text, RegistryValueKind.String);
+
+                //Prepare userinterface for copy action...
+                listBox.ResetText();
+                listBox.Items.Add("Found " + jpgFiles.Count() + " files to copy\n");
+                progressBar.Maximum = jpgFiles.Count();
+                progressBar.Value = 0;
+                progressBar.Visible = true;
+                progress.Visible = true;
+                GoButton.Text = "Cancel";
+
+                //Start copy
+                lastProgressUpdate = System.DateTime.Now;
+                bw.RunWorkerAsync();
+            }
+            else
+            {
+                listBox.Items.Add("Copy process canceled!");
+                GoButton.Text = "Go";
+                bw.CancelAsync();
+            }
+        }
+
+        //This method is executed in a separate thread created by the background worker.  
+        //so don't try to access any UI controls here!! (unless you use a delegate to do it)  
+        //this attribute will prevent the debugger to stop here if any exception is raised.  
+        //[System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            string destPath = "";
+            int i = 0;
+
+            foreach (FileInfo file in jpgFiles)
+            {
+                if ((worker.CancellationPending == true))
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                destPath = textBoxDst.Text +
+                           file.LastWriteTime.Year.ToString("0000") + "_" +
+                           file.LastWriteTime.Month.ToString("00") + "_" +
+                           file.LastWriteTime.Day.ToString("00") + "\\";
+
+                if (Directory.Exists(destPath) == false)
+                {
+                    Directory.CreateDirectory(destPath);
+                }
+                if (File.Exists(destPath + file.Name) == false)
+                {
+                    file.CopyTo(destPath + file.Name);
+                }
+                worker.ReportProgress(i++, DateTime.Now);
+            }
+            e.Result = "Copied " + i + " files.";
+        }
+
+        //This event is raised on the main thread.  
+        //It is safe to access UI controls here.  
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DateTime time = Convert.ToDateTime(e.UserState); //get additional information about progress  
+
+            progressBar.Value = e.ProgressPercentage; //update progress bar  
+            TimeSpan diff = time - lastProgressUpdate;
+
+            float speed = (float)((jpgFiles[e.ProgressPercentage].Length / (1024 * 1024)) / diff.TotalSeconds);
+            if (speed < 100)
+            {
+                progress.Text = progressBar.Value.ToString() + "/" +
+                                progressBar.Maximum.ToString() + " " +
+                                speed.ToString("0.00") + "Mb/s";
+            }
+            else
+            {
+                progress.Text = progressBar.Value.ToString() + "/" +
+                                progressBar.Maximum.ToString();
+            }
+            lastProgressUpdate = time;
+        }
+
+        //This is executed after the task is complete whatever the task has completed: 
+        //  a) sucessfully, 
+        //  b) with error,
+        //  c) has been cancelled  
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("The task has been cancelled");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("Error. Details: " + (e.Error as Exception).ToString());
+            }
+            else
+            {
+                GoButton.Text = "Go";
+                listBox.Items.Add(e.Result.ToString());
+            }
+        }
+    }
+}
